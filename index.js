442161f2bb48c89a663b789b6730900d466dc3a8
sasodoma <saso.doma@gmail.com>
2021-04-28 13:26:22 +0200

    First commit. Basic message counting has been implemented.

---
+++ index.js
@@ -0,0 +1,77 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+const client = new Discord.Client();
+const prefix = '/bzz ';
+const fetchLimit = 100;
+
+client.login(config.BOT_TOKEN);
+
+client.on('message', function(message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+    const author = message.author;
+
+    let userMessageCount = new Map();
+    let channels = [];
+    if (command === 'total') {
+        for (let channel of message.guild.channels.cache.values()) {
+            if (!channel.messages) continue;
+            channels.push(channel);
+        }
+    } else if (command === 'channel') {
+        channels.push(message.channel)
+    }
+    message.channel.send(new Discord.MessageEmbed().setColor('#f1c40f').setTitle("Started indexing")).then(reply => {
+        handleChannels(userMessageCount, channels).then(userMessageCount => {
+            message.channel.send("<@" + author.id + ">").then(sent => {
+                sent.edit(constructEmbed(userMessageCount));
+                message.delete().then(value => {}, reason => {});
+            });
+        });
+    });
+});
+
+function handleChannels(userMessageCount, channels) {
+    if (channels.length === 0) {
+        return new Promise((resolve) => {resolve(userMessageCount)});
+    }
+    let channel = channels.pop();
+    return countChannelMessages(userMessageCount, channel).then(userMessageCount => {
+        return handleChannels(userMessageCount, channels);
+    });
+}
+
+function countChannelMessages(userMessageCount, channel, lastMessage = null) {
+    return channel.messages.fetch({limit: fetchLimit, before: lastMessage}).then(messages => {
+        for (let message of messages.values()) {
+            if (!message.author) continue;
+            if (!userMessageCount.has(message.author.id)) userMessageCount.set(message.author.id, {count: 0});
+            userMessageCount.get(message.author.id).count++;
+        }
+        if(messages.size === fetchLimit) {
+            return countChannelMessages(userMessageCount, channel, messages.lastKey());
+        }
+        return userMessageCount;
+    });
+}
+
+function constructEmbed(userMessageCount){
+    let countArray = Array.from(userMessageCount);
+    countArray.sort((a, b) => {
+       return b[1].count - a[1].count;
+    });
+    let text = "";
+    for (let element of countArray) {
+        //client.users.fetch(element[0]).then(user => {});
+        text += `*<@${element[0]}>*: ${element[1].count}\n`;
+    }
+    return new Discord.MessageEmbed()
+        .setColor('#f1c40f')
+        .setTitle('Stats')
+        .setDescription(text);
+}
