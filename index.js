883cf4436565a879de25b1ff82841843fdafabcf
sasodoma <saso.doma@gmail.com>
2021-07-16 12:08:39 +0200

    Implemented pagination and some JSDocs so IDEA doesn't bug me anymore.

--- index.js
+++ index.js
@@ -92,8 +92,9 @@ client.on('message', function(message) {
                 if (userMessageCount.debug) userMessageCount.debug.endTime = new Date().getTime();
                 indexingMessage.delete().catch(logErr);
                 message.channel.send("<@" + author.id + ">").then(sent => {
-                    sent.edit(constructEmbed(userMessageCount, channelName)).catch(logErr);
                     message.delete().catch(logErr);
+                    let embeds = constructEmbed(userMessageCount, channelName);
+                    displayResults(sent, embeds, true);
                 }).catch(logErr);
             }).catch((error) => {
                 indexingMessage.delete().catch(logErr);
@@ -118,7 +119,7 @@ function countChannelMessages(userMessageCount, channel, lastMessage = null) {
         for (let message of messages.values()) {
             if (!message.author) continue;
             if (!userMessageCount.map.has(message.author.id)) {
-                userMessageCount.map.set(message.author.id, {words: 0, messages: 0, ratio: 0});
+                userMessageCount.map.set(message.author.id, {words: 0, messages: 0, ratio: 0, tag: message.author.tag});
             }
             userMessageCount.map.get(message.author.id).words += countMessageWords(message);
             userMessageCount.map.get(message.author.id).messages++;
@@ -134,50 +135,87 @@ function countMessageWords(message) {
     return message.content.split(/\s*[\s]\s*/).length;
 }
 
+/**
+ *
+ * @param {Object} userMessageCount
+ * @param {string} channelName
+ * @returns {[MessageEmbed]}
+ */
 function constructEmbed(userMessageCount, channelName){
     let countArray = Array.from(userMessageCount.map);
     countArray.sort((a, b) => {
         switch (userMessageCount.sort) {
             case 0: return b[1].messages - a[1].messages;
             case 1: return b[1].words - a[1].words;
-            case 2: return b[1].words/b[1].messages - a[1].words/a[1].messages;
+            case 2: return b[1].words / b[1].messages - a[1].words / a[1].messages;
         }
     });
-    let text = "";
+    let pages = [""];
+    let page = 0;
     for (let element of countArray) {
         let words = element[1].words;
         let messages = element[1].messages;
         let ratio = (words / messages).toFixed(3);
-        let newText = `*<@${element[0]}>*: ${messages} msgs, ${words} words, ${ratio} wpm\n`;
-        if (text.length + newText.length > 2048) break;
-        text += newText;
-    }
-    let title = userMessageCount.words ? 'Word count for ' : 'Message count for '
-    let embed = new Discord.MessageEmbed()
-        .setColor('#f1c40f')
-        .setTitle(title + channelName)
-        .setDescription(text);
-
-    if (userMessageCount.debug) {
-        let seconds = (userMessageCount.debug.endTime - userMessageCount.debug.beginTime) / 1000;
-        let minutes = Math.floor(seconds / 60);
-        let hours = Math.floor(minutes / 60);
-        let days = Math.floor(hours / 60);
-        let timeString = '';
-        if (days) timeString += days + 'd ';
-        if (hours) timeString += hours % 24 + 'h ';
-        if (minutes) timeString += minutes % 60 + 'm ';
-        if (seconds) timeString += (seconds % 60).toFixed(3) + 's';
-        let debugText = 'Total request count: ' + userMessageCount.debug.requestCount + '\n'
-            + 'Total time: ' + timeString + '\n'
-            + 'Average time per request: ' + (seconds / userMessageCount.debug.requestCount).toFixed(3)
-            + 's';
-        embed.addField('Debug info', debugText, false);
+        let newText = `**${element[1].tag}**: ${messages} msgs, ${words} words, ${ratio} wpm\n`;
+        if (pages[page].length + newText.length > 2048) pages[++page] = "";
+        pages[page] += newText;
     }
 
-    return embed;
+    let title = 'Statistics for ';
+    let embeds = [];
+    for (let page of pages) {
+        let embed = new Discord.MessageEmbed()
+            .setColor('#f1c40f')
+            .setTitle(title + channelName)
+            .setDescription(page);
+
+        if (userMessageCount.debug) {
+            let seconds = (userMessageCount.debug.endTime - userMessageCount.debug.beginTime) / 1000;
+            let minutes = Math.floor(seconds / 60);
+            let hours = Math.floor(minutes / 60);
+            let days = Math.floor(hours / 60);
+            let timeString = '';
+            if (days) timeString += days + 'd ';
+            if (hours) timeString += hours % 24 + 'h ';
+            if (minutes) timeString += minutes % 60 + 'm ';
+            if (seconds) timeString += (seconds % 60).toFixed(3) + 's';
+            let debugText = 'Total request count: ' + userMessageCount.debug.requestCount + '\n'
+                + 'Total time: ' + timeString + '\n'
+                + 'Average time per request: ' + (seconds / userMessageCount.debug.requestCount).toFixed(3)
+                + 's';
+            embed.addField('Debug info', debugText, false);
+        }
+        embeds.push(embed);
+    }
+
+    return embeds;
 }
 
+function filterReactions(react, user) {
+    return react.emoji.name === '➡' && user.id !== client.user.id;
+}
+
+/**
+ * Send the embed and add a reaction, so
+ * the user can request the next page
+ * @param {Message} message
+ * @param {[MessageEmbed]} embeds
+ * @param {boolean} edit
+ */
+function displayResults(message, embeds, edit) {
+    let sent;
+    sent = edit ? message.edit(embeds.shift()) : message.channel.send(embeds.shift());
+    if (embeds.length) sent.then(msg => msg.react('➡'))
+        .then(react => react.message.awaitReactions(filterReactions, {max: 1, time: 300000, errors: ['time']}))
+        .then(() => displayResults(message,embeds,false))
+        .then(() => sent.then(msg => msg.reactions.removeAll()))
+        .catch(logErr);
+}
+
+/**
+ * Generates the help embed
+ * @returns {module:"discord.js".MessageEmbed}
+ */
 function getHelpEmbed() {
     return new Discord.MessageEmbed()
         .setColor('#f1c40f')
